25c442fc4dca5519dd2a24e929c8eda86e50ebba
Doug Beatty <doug_beatty@yahoo.com>
2023-04-06 16:37:04 -0500

    add registration modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
-/*!
-* Start Bootstrap - Business Casual v7.0.8 (https://startbootstrap.com/theme/business-casual)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
-*/
 // Highlights current date on contact page
 window.addEventListener('DOMContentLoaded', function (event) {
         const listHoursArray = document.body.querySelectorAll('.list-hours li');
         listHoursArray[new Date().getDay()].classList.add(('today'));
     })
+// Fullcalendar.io
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
@@ -15,3 +11,17 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     calendar.render();
     });
+    $("#registerButton").click(function(){
+        $('#registerModal').modal('show');
+    });
+    $("#loginButton").click(function(){
+        $('#loginModal').modal('show');
+    });
+    function phoneFormat(input) {//returns (###) ###-####
+        input = input.replace(/\D/g,'');
+        var size = input.length;
+        if (size>0) {input="("+input}
+        if (size>3) {input=input.slice(0,4)+") "+input.slice(4,11)}
+        if (size>6) {input=input.slice(0,9)+"-" +input.slice(9)}
+        return input;
+    }
